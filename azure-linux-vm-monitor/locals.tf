1e708aef465ff32d9b00724f3bfc7014c8798691
raoulaime <aimeraoul@live.com>
2025-01-29 23:39:07 -0500

    adding some updates

--- azure-linux-vm-monitor/locals.tf
+++ azure-linux-vm-monitor/locals.tf
@@ -1,18 +1,18 @@
 locals {
-  resource_group_name = "lab-grp"
+  resource_group_name = "lab-rg"
   location            = "canadaeast"
   virtual_network = {
-    name          = "lab-network"
+    name          = "lab-vnet"
     address_space = "10.0.0.0/16"
   }
 
   subnets = [
     {
-      name           = "vm_subnet"
+      name           = "vm_snet"
       address_prefix = "10.0.0.0/24"
     },
     {
-      name           = "data_subnet"
+      name           = "data_snet"
       address_prefix = "10.0.1.0/24"
     }
   ]
