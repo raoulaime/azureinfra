31a96d6c09b6e4c49138cc2177ce05de77dfcc96
raoulaime <aimeraoul@live.com>
2025-01-31 10:23:18 -0500

    adding some updates

--- azure-backend-storage-account/backend.tf
+++ azure-backend-storage-account/backend.tf
@@ -1,25 +1,23 @@
-resource "azurerm_resource_group" "tfstate_rg" {
+resource "azurerm_resource_group" "tfbackend_rg" {
   name     = var.az_resource_group
   location = var.az_location
 }
 
-resource "azurerm_storage_account" "tfstate_sa" {
+resource "azurerm_storage_account" "tfbackend_sa" {
   name                            = var.az_storage_account
-  resource_group_name             = azurerm_resource_group.tfstate_rg.name
-  location                        = azurerm_resource_group.tfstate_rg.location
+  resource_group_name             = azurerm_resource_group.tfbackend_rg.name
+  location                        = azurerm_resource_group.tfbackend_rg.location
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
-
   tags = {
-    repo  = "ansible-terraform"
     owner = "rakit"
   }
 }
 
-resource "azurerm_storage_container" "tfsate_blob" {
+resource "azurerm_storage_container" "tfbackend_blob" {
   name                  = var.az_storage_account_container
-  storage_account_id = azurerm_storage_account.tfstate_sa.id
+  storage_account_id = azurerm_storage_account.tfbackend_sa.id
   container_access_type = "private"
 
 }
